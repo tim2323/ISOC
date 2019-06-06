3098cdf566e3a6571450f0f8214464eb43d452a5
Tim <richaudtimbis@gmail.com>
2019-06-06 10:53:17 +0200

    Ajout des dossiers MDN dépendance

---
+++ js/buttonONscroll.js
@@ -0,0 +1,18 @@
+/********************************/
+/*        BUTTON SCROLL TOP     */
+/********************************/
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 4444 || document.documentElement.scrollTop > 400) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
---
+++ js/carousel.js
@@ -0,0 +1,21 @@
+/********************************/
+/*        Carousel              */
+/********************************/
+
+// WOOOOOW AMAZING
+   wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")  // check OK
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };      
+
